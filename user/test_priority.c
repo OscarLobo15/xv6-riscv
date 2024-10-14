f0a5de98e49a5dccc6c93001f968495afe7a7c56
OscarLobo15 <oscarlobod01@gmail.com>
2024-10-14 16:27:49 -0300

    Tarea 2

--- user/test_priority.c
+++ user/test_priority.c
@@ -18,26 +18,16 @@ int main(void) {
             sleep(i * 10);
 
             // Formato de impresión del proceso hijo
-            printf("Proceso hijo %d creado, PID: %d\n", i, getpid());
+            printf("Ejecutando proceso hijo %d, PID: %d\n", i, getpid());
 
             sleep(200); // Simular trabajo con un sleep más largo
 
             // Imprimir mensaje cuando el proceso hijo termina
-            printf("Proceso hijo %d con PID: %d terminando\n", i, getpid());
+            printf(" Proceso hijo %d con PID: %d terminando\n", i, getpid());
             exit(0); // Proceso hijo termina aquí
         } else {
             // Formato de impresión del proceso padre tras crear el hijo
-            printf("Proceso padre: creó hijo %d con PID: %d\n", i, pid);
+            printf("Ejecutando proceso hijo %d con PID: %d\n", i, pid);
         }
     }
-
-    // Proceso padre espera a que todos los hijos terminen
-    for(i = 0; i < 20; i++) {
-        int child_pid = wait(0); // Esperar a que cada proceso hijo termine
-        printf("Proceso padre: hijo con PID %d ha terminado\n", child_pid);
-    }
-
-    // Imprimir mensaje cuando el proceso padre termina
-    printf("Proceso padre terminando\n");
-    exit(0); // Salida exitosa del proceso padre
 }
